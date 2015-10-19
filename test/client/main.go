e79898042d69e126c3802ad8f244c60b4c4471d5
Sarath Lakshman <sarathlakshman@slynux.com>
2015-10-19 15:55:07 +0530

    test/client: Add configurable n clients

--- test/client/main.go
+++ test/client/main.go
@@ -35,43 +35,57 @@ func main() {
 	f, _ := os.Create("prof")
 	pprof.StartCPUProfile(f)
 	defer pprof.StopCPUProfile()
-	var wg sync.WaitGroup
+	var wgc sync.WaitGroup
 	var count uint64
 
 	addr := os.Args[1]
 	n, _ := strconv.Atoi(os.Args[2])
 	thr, _ := strconv.Atoi(os.Args[3])
+	nclients, _ := strconv.Atoi(os.Args[3])
 	nperthr := n / thr
 	c, _ := squash.NewClient(addr)
 
 	t0 := time.Now()
-	tm := t0
-	for i := 0; i < thr; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for i := 0; i < nperthr; i++ {
-				p := c.NewConn()
-				p.Write(req)
-				buf := pool.Get()
-				p.Read(buf.([]byte)[:respSize])
-				pool.Put(buf)
-				p.Close()
-				x := atomic.AddUint64(&count, 1)
-				if x == 1000000 {
-					tx := time.Now()
-					fmt.Println(float64(x)/tx.Sub(tm).Seconds(), "req/sec")
-					tm = tx
-					atomic.StoreUint64(&count, 0)
-				}
 
-			}
-		}()
+	clientWorker := func(wgc *sync.WaitGroup) {
+		defer wgc.Done()
+
+		var wg sync.WaitGroup
+		tm := time.Now()
+		for i := 0; i < thr; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for i := 0; i < nperthr; i++ {
+					p := c.NewConn()
+					p.Write(req)
+					buf := pool.Get()
+					p.Read(buf.([]byte)[:respSize])
+					pool.Put(buf)
+					p.Close()
+					x := atomic.AddUint64(&count, 1)
+					if x == 1000000 {
+						tx := time.Now()
+						fmt.Println(float64(x)/tx.Sub(tm).Seconds(), "req/sec")
+						tm = tx
+						atomic.StoreUint64(&count, 0)
+					}
+
+				}
+			}()
+		}
+
+		wg.Wait()
+		tx := time.Now()
+		fmt.Println(float64(count)/tx.Sub(tm).Seconds(), "req/sec")
 	}
 
-	wg.Wait()
+	for i := 0; i < nclients; i++ {
+		wgc.Add(1)
+		go clientWorker(&wgc)
+	}
 
-	tx := time.Now()
-	fmt.Println(float64(count)/tx.Sub(tm).Seconds(), "req/sec")
-	fmt.Println(float64(n)/tx.Sub(t0).Seconds(), "req/sec")
+	wgc.Wait()
+
+	fmt.Println(float64(n)/time.Since(t0).Seconds(), "req/sec")
 }
